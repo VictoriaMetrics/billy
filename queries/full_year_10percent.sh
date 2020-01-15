3442b90ab1873285a2c74978041082a6d4a7fef8
Aliaksandr Valialkin <valyala@gmail.com>
2020-01-15 16:44:50 +0200

    queries: make queries independent on the number of time series

--- queries/full_year_10percent.sh
+++ queries/full_year_10percent.sh
@@ -1,1 +1,1 @@
-curl -G http://localhost:8428/api/v1/query -d "time=$(date -d '2020-01-01 00:00:00 UTC' +%s)" -d 'query=max(max_over_time(temperature{sensor_id=~"1[0-9]{5}"}[366d]))'
+curl -G http://localhost:8428/api/v1/query -d "time=$(date -d '2020-01-01 00:00:00 UTC' +%s)" -d 'query=max(max_over_time(temperature{sensor_id=~"1.*"}[366d]))'
