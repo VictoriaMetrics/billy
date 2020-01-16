d495c28bdcdd66bb77c564c326b1090b760db1f7
Aliaksandr Valialkin <valyala@gmail.com>
2020-01-16 19:38:01 +0200

    scripts/start_victoria_metrics.sh: add search.maxQueryDuration and search.maxUniqueTimeseries

--- scripts/start_victoria_metrics.sh
+++ scripts/start_victoria_metrics.sh
@@ -4,4 +4,4 @@ ARCHIVE=victoria-metrics-$VERSION.tar.gz
 test -f $ARCHIVE || curl -L https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/$VERSION/$ARCHIVE > $ARCHIVE
 tar xzf $ARCHIVE
 ulimit -n 100000
-./victoria-metrics-prod -retentionPeriod=200 -storageDataPath=/mnt/disks/billy/victoria-metrics-data
+./victoria-metrics-prod -retentionPeriod=200 -storageDataPath=/mnt/disks/billy/victoria-metrics-data -search.maxQueryDuration=24h -search.maxUniqueTimeseries=2000000
