c9ad15af4605f4a7d68feb2fdedb71b8e5bc303a
Aliaksandr Valialkin <valyala@gmail.com>
2020-01-18 00:03:05 +0200

    use github.com/klauspost/compress/gzip instead of standard compress/gzip for better performance

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/VictoriaMetrics/billy
+
+go 1.12
+
+require github.com/klauspost/compress v1.9.7
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"compress/gzip"
 	"flag"
 	"fmt"
 	"io"
@@ -15,6 +14,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/klauspost/compress/gzip"
 )
 
 var (
